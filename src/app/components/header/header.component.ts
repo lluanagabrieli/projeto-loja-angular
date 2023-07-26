be8a18c6e6d37b6be9eea4fb58fbffd36d7f12b2
lluanagabrieli <luanagabrieli4599@hotmail.com>
2023-07-26 16:33:36 -0300

    adicionando repositório

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent {
+
+    constructor(){
+
+    }
+
+    headerToggle(){
+        const navegacao = document.querySelector('#navegacao')
+
+    }
+    }
