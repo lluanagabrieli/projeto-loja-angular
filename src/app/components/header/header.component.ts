c5e82eb3c1d3e817d0e0cac90ae98068ba958156
lluanagabrieli <luanagabrieli4599@hotmail.com>
2023-07-27 19:51:51 -0300

    criação de método para evento no menu hamburguer

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -6,12 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
+// TypeScript
+    private cabecalhoMobile: HTMLElement;
+    private cabecalhoNavegacao: HTMLElement;
 
     constructor(){
-
+        this.cabecalhoMobile = document.querySelector(".cabecalho-navegacao-mobile")!;
+        this.cabecalhoNavegacao = document.querySelector(".cabecalho-navegacao")!;
+        this.click();
     }
 
-    cabecalhoToggler(){
-        const navegacao = document.querySelector('#navegacao')
-        }
+    click(){
+            if (this.cabecalhoMobile && this.cabecalhoNavegacao) {
+                this.cabecalhoMobile.addEventListener("click", () => {
+                this.cabecalhoNavegacao.classList.toggle("active");
+                });
+            }
+            };
     }
+ const instancia = new HeaderComponent();
