2591a8a4635857397606a3b50210a5787808847f
lluanagabrieli <luanagabrieli4599@hotmail.com>
2023-07-31 18:02:36 -0300

    feat(componentes): adiciona novos componentes e importa os novos componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { PrimeiraSecaoComponent } from './components/primeira-secao/primeira-secao.component';
+import { SegundaSecaoComponent } from './components/segunda-secao/segunda-secao.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    PrimeiraSecaoComponent,
+    SegundaSecaoComponent
   ],
   imports: [
     BrowserModule,
