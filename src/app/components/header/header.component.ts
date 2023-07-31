019eab73b768c212617624b029625c8593f6de16
lluanagabrieli <luanagabrieli4599@hotmail.com>
2023-07-31 17:59:20 -0300

    feat(comentario): adição de comentários para entendimento da lógica

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -6,9 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
+    // variável atribuída à classe '.active'
     isMenuOpen = false;
 
     toggleMenu() {
         this.isMenuOpen = !this.isMenuOpen;
+        // ao clicar na div com o íconezinho de menu, esse método vai ser chamado, fazendo com que o valor que era falso se tornar verdadeiro, ativando a classe .active. O contrário tb acontece.
     }
 }
